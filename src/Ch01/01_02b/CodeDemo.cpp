2ecf3f76f016503d7ba16979f8585cbef03f7705
h4xter1612 <88903273+h4xter1612@users.noreply.github.com>
2025-12-10 18:02:30 +0000

    1-2B

--- src/Ch01/01_02b/CodeDemo.cpp
+++ src/Ch01/01_02b/CodeDemo.cpp
@@ -1,3 +1,12 @@
 // Complete Guide to C++ Programming Foundations
 // Exercise 01_02
 // Hello World, by Eduardo Corpeño 
+# include <iostream>
+
+int main()
+{
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
+
+// End of CodeDemo.cpp
